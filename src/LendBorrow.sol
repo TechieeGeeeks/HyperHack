a5ddef3771305bd8901bdc43b24131f05527a833
Swayam Karle <swayamkarle_ce_2020@ltce.in>
2023-10-16 02:54:28 +0530

    Rough Code is Done

--- src/LendBorrow.sol
+++ src/LendBorrow.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.18;
 
 /* Imports Here */
 import {IBridge} from "./IBridge.sol";
-import {IERC71} from "lib/openzeppelin-contracts/contracts/token/ERC721";
+import {IERC721} from "lib/openzeppelin-contracts/contracts/token/ERC721";
 import {DUSD} from "./DUSD.sol";
 
 
@@ -14,7 +14,9 @@ contract LendBorrow {
     error LendBorrow_ContractIsNotAllowedToPullToken();
     error LendBorrow_TokenDoesNotHaveWorthInContrat();
     error LendBorrow_UserDoesNotHaveBorrowingPower();
-
+    error LendBorrow_NFTDoesNotLendOnThisChain();
+    error LendBorrow_UserDoesNotHaveSufficientTokensToBurn();
+    error LendBorrow_UserDoesNotHaveAnyLoanPending();
     /* Type Declarations */
     struct OriginalToken {
         address tokenAddress;
@@ -33,6 +35,7 @@ contract LendBorrow {
     address payable public bridge;
     address payable public dusd_Address;
     DUSD public dusdContract;
+    address public DAO_CONTRACT_ADDRESS = 0x017c6CdD043aEF7e3F4400362CbE0dE0D2Cfd050;
 
     // Using it so that user can get back his NFT after repaying loan
     mapping(address => OriginalToken) public ownerOfOrignalTokens;
@@ -48,6 +51,8 @@ contract LendBorrow {
 
     // Using it for deposit NFT check meanswe are only allowing blue chip nfts so only this listed tokens can take loan
     mapping(address=>mapping(uint256=>uint256)) public contractAddressToTokenIdToDUSDBorrowableAmount;
+
+    mapping(bytes32=>uint256)public bytes32OfTokenToFloorPriceOfTokenAtTimeOfDepositing;
 
     /* Constructor */
     constructor(address _owner) {
@@ -66,13 +71,19 @@ contract LendBorrow {
         bridge = _bridge;
     }
 
-
-    function setTokenValue(address tokenContractAddress, uint256 tokenID, uint256 DUSD_NFT_WORTH) external{
+    function floorPriceOfNFT(address tokenContractAddress, uint256 tokenID, uint256 DUSD_NFT_WORTH) external returns(DUSD_NFT_WORTH){
         if(msg.sender != i_owner){
             revert LendBorrow_AddressIsNotOwner_Error();
         }
-        contractAddressToTokenIdToDUSDBorrowableAmount[tokenContractAddress][tokenID]= DUSD_NFT_WORTH;
+        bytes32 tokenBytes32Value =calculateBytes32ValueFromTokenWithoutBorrower(tokenID,tokenContractAddress);
 
+        bytes32OfTokenToFloorPriceOfTokenAtTimeOfDepositing[tokenBytes32Value] = DUSD_NFT_WORTH;
+        return(DUSD_NFT_WORTH);
+
+    }
+
+    function setTokenValue(address tokenContractAddress, uint256 tokenID) external{
+        contractAddressToTokenIdToDUSDBorrowableAmount[tokenContractAddress][tokenID]= floorPriceOfNFT(tokenContractAddress,tokenID)*0.7;
     }
 
     // Native Chain function only
@@ -120,7 +131,7 @@ contract LendBorrow {
                     DUSD_LOAN_AMOUNT,
                     msg.sender, // msg.sender can get this much DUSD
                     msg.sender // Refund Address
-                )
+                );
             }
         }
     }
@@ -133,43 +144,180 @@ contract LendBorrow {
         }
 
         // Get the borowing power and mint usd on that borrowers address
-        uint256 loanUSD = borrowingPowerInUSD[msg.sender];
-        dusdContract.mint(msg.sender,loanUSD);
+        uint256 loanDUSD = borrowingPowerInUSD[msg.sender];
+        dusdContract.mint(msg.sender,loanDUSD);
+
+        if(ownerOfOrignalTokens[msg.sender].tokenId != 0){
+            makeReadyToAcceptLoanAmountWhenTokensWherePulledFromMainChain(msg.sender,loanDUSD);
+        }else{
+            //Reset the borrowing power
+            borrowingPowerInUSD[msg.sender]=0;
+            // Removing borrowing power on all chain
+            for (uint i = 0; i < chainIds.length; i++){
+                // Except this chain execute call for all two remaining chains
+                if(block.chainid != chainIds[i]){
+                    uint32 chainId = chainIds[i];
+                    uint256 fee = IBridge(bridge).quoteFeeAddBorrowingPowerSend(chainId);
+                    IBridge(bridge).RemoveBorrowingPowerSend{value:fee}(
+                        chainId,
+                        loanDUSD,
+                        msg.sender, // msg.sender can get this much DUSD
+                        msg.sender // Refund Address
+                    );
+                }
+            }
+        }
+    }
+
+    function addBorrowingPowerByBridge(address borrowerAddress,uint256 DUSD_AMOUNT,uint32 _origin) external{
+        require(
+            msg.sender == bridge,
+            "ERC721Multichain: Only bridge can call this function"
+        );
+        // Update the borrowing power on native chain
+        borrowingPowerInUSD[borrowerAddress] = DUSD_AMOUNT;
+    }
+
+    function removeBorrowingPowerByBridge(address borrowerAddress,uint256 DUSD_AMOUNT,uint32 _origin) external{
+        require(
+            msg.sender == bridge,
+            "ERC721Multichain: Only bridge can call this function"
+        );
+        
+        // Check if the Borrower has borrowing power or not
+        uint256 loanDUSD = borrowingPowerInUSD[borrowerAddress];
+        if(loanDUSD==0){
+            revert LendBorrow_UserDoesNotHaveBorrowingPower();
+        }
+
+        // Just removing borrowing power so that borrower should not be able to withdraw tokens
+
+        //Reset the borrowing power
+        borrowingPowerInUSD[borrowerAddress]=0;
+
+        // Ok we removed the borrowing power but check if the borrower lended the nft on this chain only? if yes then we have to manipulate other mappings too
+        if(ownerOfOrignalTokens[borrowerAddress].tokenId != 0){
+            makeReadyToAcceptLoanAmountWhenTokensWherePulledFromOtherChain(borrowerAddress,loanDUSD);
+        }
+
+    }
+
+    function makeReadyToAcceptLoanAmountWhenTokensWherePulledFromMainChain(address borrowerAddress, uint256 loanDUSD) internal{
 
         // Associate Loan with borrowers address
-        addressToAssociatedLoan[msg.sender]=loanUSD;
+        addressToAssociatedLoan[borrowerAddress]=loanDUSD;
 
         // Check which token he putted as collateral and grab tokenAddress and Token Id from it
-        OriginalToken memory token = ownerOfOrignalTokens[msg.sender];
-        address tokenContractAddress = token.tokenAddress;
-        uint256 token_id = token.tokenId;
-        // Then take the bytes32 of abi.encode(token_id,tokenContractAddress) and associate loan to NFT cause ehen any user wanna liquidate nft we can use this info to calculate HF in liquidation.
-        bytes32OfTokenToAssociatedLoan[bytes32(abi.enocde(token_id,tokenContractAddress))] = loanUSD;
+        bytes32 bytes32OftokenAndTokenContractAddress = calculateBytes32ValueFromTokenIdAndTokenContractAddress(borrowerAddress);
 
+        // Then take the bytes32 of abi.encode(token_id,tokenContractAddress) and associate loan to NFT cause ehen any user wanna liquidate nft we can use this info to calculate HF in liquidation.
+        bytes32OfTokenToAssociatedLoan[bytes32OftokenAndTokenContractAddress] = loanDUSD;
+  
         //Reset the borrowing power
         borrowingPowerInUSD[msg.sender]=0;
 
         // Removing borrowing power on all chain
-        for (uint i = 0; i < chainIds.length; i++){
+        for (uint256 i = 0; i < chainIds.length; i++){
             // Except this chain execute call for all two remaining chains
             if(block.chainid != chainIds[i]){
                 uint32 chainId = chainIds[i];
                 uint256 fee = IBridge(bridge).quoteFeeAddBorrowingPowerSend(chainId);
                 IBridge(bridge).RemoveBorrowingPowerSend{value:fee}(
                     chainId,
-                    loanUSD,
+                    loanDUSD,
                     msg.sender, // msg.sender can get this much DUSD
                     msg.sender // Refund Address
                 );
             }
         }
+
     }
 
-    function withDrawTokensByBridge() external{}
-    function addBorrowingPowerByBridge() external{}
-    function removeBorrowingPowerByBridge() external{}
-    function repayLoan() external{}
-    function liquidateNFTByTransferingTheOwnerShipToDaoContract() internal{}
-    function tryToLiquidateNFT() external{}
+    function makeReadyToAcceptLoanAmountWhenTokensWherePulledFromOtherChain(address borrowerAddress, uint256 loanDUSD) internal{
+
+        // Associate Loan with borrowers address
+        addressToAssociatedLoan[borrowerAddress]=loanDUSD;
+
+        // Check which token he putted as collateral and grab tokenAddress and Token Id from it
+        bytes32 bytes32OftokenAndTokenContractAddress = calculateBytes32ValueFromTokenIdAndTokenContractAddress(borrowerAddress);
+        // Then take the bytes32 of abi.encode(token_id,tokenContractAddress) and associate loan to NFT cause ehen any user wanna liquidate nft we can use this info to calculate HF in liquidation.
+        bytes32OfTokenToAssociatedLoan[bytes32OftokenAndTokenContractAddress] = loanDUSD;
+
+    }
+
+    function repayLoan() external{
+        if (ownerOfOrignalTokens[msg.sender].tokenId == 0) {
+            revert LendBorrow_NFTDoesNotLendOnThisChain();
+        }
+        if(addressToAssociatedLoan[msg.sender]>0){
+            revert LendBorrow_UserDoesNotHaveAnyLoanPending();
+        }
+        uint256 loanToPay = addressToAssociatedLoan[msg.sender];
+
+        if(dusdContract.balanceOf(msg.sender)>=loanToPay){
+            revert LendBorrow_UserDoesNotHaveSufficientTokensToBurn();
+        }
+        dusdContract.burn(msg.sender, loanToPay);
+
+        // Move NFT back to msg.msg.sender
+        IERC721(ownerOfOrignalTokens[msg.sender].tokenAddress).safeTransferFrom(
+            address(this),
+            msg.sender,
+            ownerOfOrignalTokens[msg.sender].tokenId
+        );
+
+        addressToAssociatedLoan[msg.sender] = 0;
+
+        bytes32 bytes32OftokenAndTokenContractAddress = calculateBytes32ValueFromTokenIdAndTokenContractAddress(msg.sender);
+
+        bytes32OfTokenToAssociatedLoan[bytes32OftokenAndTokenContractAddress] = 0 ;
+
+        ownerOfOrignalTokens[msg.sender] = OriginalToken(address(0), 0);   
+        
+    }
+
+    function liquidateNFTByTransferingTheOwnerShipToDaoContract(uint256 token_id,address tokenContractAddress) internal returns(string memory ){
+        // We need 3 DAO contracts on 3 chains but right now i am using a normal EOA address to move my NFT
+        // Move NFT back to msg.msg.sender
+
+        IERC721(tokenContractAddress).safeTransferFrom(
+            address(this),
+            DAO_CONTRACT_ADDRESS,
+            token_id
+        );
+        return("NFT Liauidated");
+    }
+
+    function tryToLiquidateNFT(uint256 token_id , address tokenContractAddress) external returns(string memory){
+
+        bytes32 bytes32OftokenAndTokenContractAddress = calculateBytes32ValueFromTokenWithoutBorrower(token_id,tokenContractAddress);
+
+        uint256 NFTValueAtTimeOfDeposit = bytes32OfTokenToFloorPriceOfTokenAtTimeOfDepositing[bytes32OftokenAndTokenContractAddress];
+
+        uint256 NFTValueRightNow = NFTValueAtTimeOfDeposit - (NFTValueAtTimeOfDeposit*0.1);
+
+        uint256 healthFactor = NFTValueAtTimeOfDeposit/NFTValueRightNow;
+
+        if(healthFactor>1){
+            string memory status = liquidateNFTByTransferingTheOwnerShipToDaoContract(token_id,tokenContractAddress);
+            return status;
+        }else{
+            return("Token Cannot Be Liquidated");
+        }
+
+    }
+
+    function calculateBytes32ValueFromTokenIdAndTokenContractAddress(address borrowerAddress) internal returns(bytes32){
+        OriginalToken memory token = ownerOfOrignalTokens[borrowerAddress];
+        address tokenContractAddress = token.tokenAddress;
+        uint256 token_id = token.tokenId;
+        bytes32 bytes32OfTokenIdAndTokenAddress = bytes32(abi.encode(token_id,tokenContractAddress));
+        return(bytes32OfTokenIdAndTokenAddress);
+    }
+
+    function calculateBytes32ValueFromTokenWithoutBorrower(uint256 token_id , address tokenContractAddress) internal returns(bytes32){
+        bytes32 bytes32OfTokenIdAndTokenAddress = bytes32(abi.encode(token_id,tokenContractAddress));
+        return(bytes32OfTokenIdAndTokenAddress);
+    }
 
 }

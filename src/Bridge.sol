e4e9b8eeebe7b78168c7ea95691add77c16a3fcc
Swayam Karle <swayamkarle_ce_2020@ltce.in>
2023-10-17 04:34:45 +0530

    Receive function and contract ethers pulling function added

--- src/Bridge.sol
+++ src/Bridge.sol
@@ -17,7 +17,7 @@ contract Bridge {
     IInterchainGasPaymaster public igp;
     LendBorrow public lendBorrow;
 
-    constructor(address _mailbox, address _igp, address _lendBorrow) {
+    constructor(address _mailbox, address _igp, address payable _lendBorrow) {
         mailbox = IMailbox(_mailbox);
         igp = IInterchainGasPaymaster(_igp);
         lendBorrow = LendBorrow(_lendBorrow);

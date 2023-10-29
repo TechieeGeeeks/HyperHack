bdd3c94ace3a7281cbc192b8d4dfc68a3fd79499
Swayam <swayamkarle_ce_2020@ltce.in>
2023-10-29 11:32:22 +0530

    Contract Config

---
+++ client/contract_Config/contract_Config/POLYGON/BridgeConfig.js
@@ -0,0 +1,243 @@
+const BridgeContractAddressOnPolygon = 0x652ab1349bB6e29Aea578bd618A170C6435AAc38;
+
+const BridgeContractABIsOnPolygon = [
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_mailbox",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_igp",
+				"type": "address"
+			},
+			{
+				"internalType": "address payable",
+				"name": "_lendBorrow",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint32",
+				"name": "_destination",
+				"type": "uint32"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_DUSD_AMOUNT",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "_borrower",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_refundAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_destinationBridge",
+				"type": "address"
+			}
+		],
+		"name": "AddBorrowingPowerSend",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint32",
+				"name": "_destination",
+				"type": "uint32"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_DUMMY_NUMBER",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "_borrower",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_refundAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_destinationBridge",
+				"type": "address"
+			}
+		],
+		"name": "GiveBackOwnershipOfNFTOnAllChains",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint32",
+				"name": "_destination",
+				"type": "uint32"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_DUSD_AMOUNT",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "_borrower",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_refundAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_destinationBridge",
+				"type": "address"
+			}
+		],
+		"name": "RemoveBorrowingPowerSend",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint32",
+				"name": "_origin",
+				"type": "uint32"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "_sender",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "bytes",
+				"name": "_message",
+				"type": "bytes"
+			}
+		],
+		"name": "handle",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "igp",
+		"outputs": [
+			{
+				"internalType": "contract IInterchainGasPaymaster",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "lendBorrow",
+		"outputs": [
+			{
+				"internalType": "contract LendBorrow",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "mailbox",
+		"outputs": [
+			{
+				"internalType": "contract IMailbox",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint32",
+				"name": "destination",
+				"type": "uint32"
+			}
+		],
+		"name": "quoteFeeAddBorrowingPowerSend",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint32",
+				"name": "destination",
+				"type": "uint32"
+			}
+		],
+		"name": "quoteFeeGiveBackNFTOwnership",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint32",
+				"name": "destination",
+				"type": "uint32"
+			}
+		],
+		"name": "quoteFeeRemoveBorrowingPowerSend",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]

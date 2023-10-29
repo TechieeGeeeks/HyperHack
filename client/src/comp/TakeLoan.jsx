b70b45cc15bfaf5582c0d3710fb73613791e57ea
Swayam <swayamkarle_ce_2020@ltce.in>
2023-10-30 01:35:24 +0530

    Borrowing Power Integration

--- client/src/comp/TakeLoan.jsx
+++ client/src/comp/TakeLoan.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 const { ethers } = require("ethers");
 
 const TakeLoan = ({ signer, mainConfig, chainId }) => {
   const [tokenUri, setTokenUri] = useState("");
   const [tokenCounter, setTokenCounter] = useState(null);
-  const [customTokenID, setCustomTokenID] = useState(""); // State to hold the custom tokenID value
+  const [customTokenID, setCustomTokenID] = useState("");
+  const [depositTokenID, setDepositTokenID] = useState(""); 
+  const [borrowingPower, setBorrowingPower] = useState("");
 
   const mintNft = async () => {
     try {
@@ -28,7 +30,6 @@ const TakeLoan = ({ signer, mainConfig, chainId }) => {
   const approveAddress = async () => {
     try {
       const lendBorrowAddress = mainConfig.lendBorrow.address;
-      // Use the customTokenID directly in the approval
       const tx = await mainConfig.basicNFT.approve(
         lendBorrowAddress,
         customTokenID
@@ -40,45 +41,70 @@ const TakeLoan = ({ signer, mainConfig, chainId }) => {
     }
   };
 
-  return (
-    <div className=" max-w-6xl mx-auto ">
+  const depositNFT = async () => {
+    try {
+      const tokenContractAddress = mainConfig.basicNFT.address; // Replace with the actual contract address
+      console.log(tokenContractAddress);
+      const tx = await mainConfig.lendBorrow.depositNFT(
+        tokenContractAddress,
+        depositTokenID
+      );
+      await tx.wait();
+      console.log("NFT deposited successfully for Token ID:", depositTokenID);
+    } catch (error) {
+      console.error("Error depositing NFT:", error);
+    }
+  };
 
+  const checkBorrowingPower = async () => {
+    try {
+      // Call the contract's borrowingPowerInUSD function to get the borrowing power of the provided address
+      const addressToCheck = await signer.getAddress();
+      const borrowingPower = await mainConfig.lendBorrow.borrowingPowerInUSD(
+        addressToCheck
+      );
+      const borrowingPowerInDecimal = parseInt(borrowingPower._hex, 16);
+      setBorrowingPower(borrowingPowerInDecimal);
+      console.log(
+        `Borrowing power for address ${addressToCheck}: ${borrowingPower}`
+      );
+      // You can display the borrowing power or perform any other actions as needed.
+    } catch (error) {
+      console.error("Error checking borrowing power:", error);
+    }
+  };
+
+  return (
+    <div className="max-w-6xl mx-auto">
       {(chainId === 80001 && (
-        <h1 className="  text-xl text-center text-white mt-12">
+        <h1 className="text-xl text-center text-white mt-12">
           Active Chain is{" "}
-          <span className=" font-semibold text-red-500 ">Polygon</span>
+          <span className="font-semibold text-red-500">Polygon</span>
         </h1>
       )) ||
         (chainId === 11155111 && (
-          <h1 className="  text-xl text-center text-white mt-12">
+          <h1 className="text-xl text-center text-white mt-12">
             Active Chain is{" "}
-            <span className=" font-semibold text-red-500 ">Sepolia</span>
+            <span className="font-semibold text-red-500">Sepolia</span>
           </h1>
         ))}
 
-      {/* <h1 className="  text-xl text-center text-white mt-12">
-        Active Chain is{" "}
-        <span className=" font-semibold text-red-500 ">Sepolia</span>
-      </h1> */}
-
       <div className="shadow-[0.8px_0.8px_1px_1px_rgba(143,255,106,1)] flex flex-col gap-3 text-center p-5 px-6 max-w-6xl rounded-lg mx-auto mt-12">
-        <h2 className=" font-semibold text-xl text-primaryColor">
-          Mint an NFT
-        </h2>
+        <h2 className="font-semibold text-xl text-primaryColor">Mint an NFT</h2>
         <input
           type="text"
           placeholder="Token URI"
           value={tokenUri}
           onChange={(e) => setTokenUri(e.target.value)}
-          className=" border border-subtitleColor focus:outline-none bg-transparent rounded-lg px-4 py-1"
+          className="border border-subtitleColor focus:outline-none bg-transparent rounded-lg px-4 py-1"
         />
-        <button onClick={mintNft} className=" bg-cardBg rounded-lg px-4 py-1">
+        <button onClick={mintNft} className="bg-cardBg rounded-lg px-4 py-1">
           Mint NFT
         </button>
       </div>
 
       <div className="shadow-[0.8px_0.8px_1px_1px_rgba(143,255,106,1)] flex flex-col gap-3 text-center p-5 px-6 max-w-6xl rounded-lg mx-auto mt-12">
-        <h2 className=" font-semibold text-xl text-primaryColor">
+        <h2 className="font-semibold text-xl text-primaryColor">
           Get The Latest Count of Available NFT
         </h2>
         <p className="text-subtitleColor">
@@ -87,14 +113,14 @@ const TakeLoan = ({ signer, mainConfig, chainId }) => {
         </p>
         <button
           onClick={fetchTokenCounter}
-          className=" bg-cardBg rounded-lg px-4 py-1"
+          className="bg-cardBg rounded-lg px-4 py-1"
         >
           Get Latest Token Counter
         </button>
       </div>
 
       <div className="shadow-[0.8px_0.8px_1px_1px_rgba(143,255,106,1)] flex flex-col gap-3 text-center p-5 px-6 max-w-6xl rounded-lg mx-auto mt-12">
-        <h2 className=" font-semibold text-xl text-primaryColor">
+        <h2 className="font-semibold text-xl text-primaryColor">
           Approve Lend Borrow Contract to pull NFT
         </h2>
         <input
@@ -102,25 +128,58 @@ const TakeLoan = ({ signer, mainConfig, chainId }) => {
           placeholder="Nft Token ID"
           value={customTokenID}
           onChange={(e) => setCustomTokenID(e.target.value)}
-          className=" border border-subtitleColor focus:outline-none bg-transparent rounded-lg px-4 py-1"
+          className="border border-subtitleColor focus:outline-none bg-transparent rounded-lg px-4 py-1"
         />
         <button
           onClick={approveAddress}
-          className=" bg-cardBg rounded-lg px-4 py-1"
+          className="bg-cardBg rounded-lg px-4 py-1"
         >
           Approve
         </button>
       </div>
 
+      <h1 className="text-xl text-center text-white mt-12">
+        <span className="font-semibold text-red-500">
+          Wait for Owner to WhiteList Your NFT
+        </span>
+      </h1>
+
       <div className="shadow-[0.8px_0.8px_1px_1px_rgba(143,255,106,1)] flex flex-col gap-3 text-center p-5 px-6 max-w-6xl rounded-lg mx-auto mt-12">
-        <p style={{ color: "red" }}>Setting Up Floor price of NFT</p>
-      </div>  
+        <h2 className="font-semibold text-xl text-primaryColor">Deposit NFT</h2>
+        <input
+          type="text"
+          placeholder="Deposit Token ID"
+          value={depositTokenID}
+          onChange={(e) => setDepositTokenID(e.target.value)}
+          className="border border-subtitleColor focus:outline-none bg-transparent rounded-lg px-4 py-1"
+        />
+        <button onClick={depositNFT} className="bg-cardBg rounded-lg px-4 py-1">
+          Deposit NFT
+        </button>
+      </div>
 
+      <h1 className="text-xl text-center text-white mt-12">
+        <span className="font-semibold text-red-500">
+          Waiting For Multi Chain call approx 3mins
+        </span>
+        <br />
+        <span className="font-semibold text-green-500">
+          Now You Can switch chain and check if you can borrow tokens or not
+        </span>
+      </h1>
 
-      <div>
+      <div className="shadow-[0.8px_0.8px_1px_1px_rgba(143,255,106,1)] flex flex-col gap-3 text-center p-5 px-6 max-w-6xl rounded-lg mx-auto mt-12">
+        <h2 className="font-semibold text-xl text-primaryColor">
+          Borrowing Power is {borrowingPower}
+        </h2>
         
-      </div> 
-
+        <button
+          onClick={() => checkBorrowingPower()}
+          className="bg-cardBg rounded-lg px-4 py-1"
+        >
+          Check Borrowing Power
+        </button>
+      </div>
     </div>
   );
 };

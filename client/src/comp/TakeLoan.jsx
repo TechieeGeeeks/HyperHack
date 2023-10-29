7e2aa85003ad6e1881993a90afb9050e705c6500
Swayam <swayamkarle_ce_2020@ltce.in>
2023-10-30 00:20:07 +0530

    3 functions are done

--- client/src/comp/TakeLoan.jsx
+++ client/src/comp/TakeLoan.jsx
@@ -4,6 +4,7 @@ const { ethers } = require("ethers");
 const TakeLoan = ({ signer, mainConfig }) => {
   const [tokenUri, setTokenUri] = useState("");
   const [tokenCounter, setTokenCounter] = useState(null);
+  const [customTokenID, setCustomTokenID] = useState(""); // State to hold the custom tokenID value
 
   const mintNft = async () => {
     try {
@@ -21,13 +22,30 @@ const TakeLoan = ({ signer, mainConfig }) => {
   const fetchTokenCounter = async () => {
     const latestCount = await mainConfig.basicNFT.getTokenCounter();
     const decimalNumber = parseInt(latestCount._hex, 16);
-    setTokenCounter(2);
+    setTokenCounter(decimalNumber);
+  };
+
+  const approveAddress = async () => {
+    try {
+      const lendBorrowAddress = mainConfig.lendBorrow.address;
+      // Use the customTokenID directly in the approval
+      const tx = await mainConfig.basicNFT.approve(
+        lendBorrowAddress,
+        customTokenID
+      );
+      await tx.wait();
+      console.log("Address approved successfully for Token ID:", customTokenID);
+    } catch (error) {
+      console.error("Error approving address:", error);
+    }
   };
 
   return (
     <div className=" ">
-      <div className="   border flex flex-col gap-3 text-center p-5 px-6 max-w-6xl rounded-lg mx-auto mt-12">
-        <h2 className=" font-semibold text-xl text-primaryColor">Mint an NFT</h2>
+      <div className="border flex flex-col gap-3 text-center p-5 px-6 max-w-6xl rounded-lg mx-auto mt-12">
+        <h2 className=" font-semibold text-xl text-primaryColor">
+          Mint an NFT
+        </h2>
         <input
           type="text"
           placeholder="Token URI"
@@ -38,7 +56,15 @@ const TakeLoan = ({ signer, mainConfig }) => {
         <button onClick={mintNft} className=" bg-cardBg rounded-lg px-4 py-1">
           Mint NFT
         </button>
-        <p className=" text-subtitleColor">
+
+        
+      </div>
+
+      <div className="border flex flex-col gap-3 text-center p-5 px-6 max-w-6xl rounded-lg mx-auto mt-12">
+        <h2 className=" font-semibold text-xl text-primaryColor">
+          Get The Latest Count of Available NFT
+        </h2>
+        <p className="text-subtitleColor">
           Current Token Counter is{" "}
           <span className="text-white font-medium">{tokenCounter}</span>
         </p>
@@ -47,6 +73,25 @@ const TakeLoan = ({ signer, mainConfig }) => {
           className=" bg-cardBg rounded-lg px-4 py-1"
         >
           Get Latest Token Counter
+        </button>
+      </div>
+
+      <div className="border flex flex-col gap-3 text-center p-5 px-6 max-w-6xl rounded-lg mx-auto mt-12">
+        <h2 className=" font-semibold text-xl text-primaryColor">
+          Approve Lend Borrow Contract to pull NFT
+        </h2>
+        <input
+          type="text"
+          placeholder="Nft Token ID"
+          value={customTokenID}
+          onChange={(e) => setCustomTokenID(e.target.value)}
+          className=" border bg-transparent rounded-lg px-4 py-1"
+        />
+        <button
+          onClick={approveAddress}
+          className=" bg-cardBg rounded-lg px-4 py-1"
+        >
+          Approve
         </button>
       </div>
     </div>

7fb7cd126967862f1055df7104b09b916221ace3
Swayam <swayamkarle_ce_2020@ltce.in>
2023-10-30 03:24:18 +0530

    Everything is good

--- client/src/comp/LiauidateNFT.jsx
+++ client/src/comp/LiauidateNFT.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const LiauidateNFT = () => {
   return (
-    <div>LiauidateNFT</div>
-  )
-}
+    <div>
+      <p>
+        Right Now the Main Issue is We are setting up the NFT Worth Manually so
+        But on production we will nbe doing it via chainlink data feed for floor
+        price of NFT
+      </p>
+      <p>
+        So to liquidate the NFT we need a live price checker of NFT which is abscent right now but we still wrote logic for it so the implmentation is not shown
+      </p>
+    </div>
+  );
+};
 
-export default LiauidateNFT
+export default LiauidateNFT;
